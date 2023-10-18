89b43973deed906f066007df6c281debd4508d84
takumiya081 <takumiya081@gmail.com>
2023-10-18 17:20:15 +0900

    setting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "printWidth": 100,
+  "singleQuote": true,
+  "semi": true,
+  "trailingComma": "all",
+  "bracketSpacing": false,
+  "arrowParens": "always",
+  "endOfLine": "lf"
+}
